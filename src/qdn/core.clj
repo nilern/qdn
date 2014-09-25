1d68387a967ad46746b52473691e46e4f196d98b
Pauli Jaakkola <pauli.jaakkola@iki.fi>
2014-09-25 18:49:08 +0300

    Basic functionality is quite nice.
    * 11 passing tests
    * marginalia documentation

--- src/qdn/core.clj
+++ src/qdn/core.clj
@@ -1,79 +1,116 @@
 (ns qdn.core
-  (:require [qdn.util :refer :all]
-            [clojure.string :as string]
-            [clojure.edn :as edn]
-            [cljs.analyzer :as ana]
-            [cljs.compiler :as c]))
-
-;;; * TODO: clean up everything
-
-;;; Compiling
-;;; ============================================================================
-
-(defn compile-expr [s]
-  (with-out-str (c/emit (ana/analyze {} s))))
+  "Turn (hiccup-like) edn forms into QML."
+  (:require [clojure.string :as string]))
 
 ;;; Import section
 ;;; ============================================================================
 
-(defn import-vec->str [iv]
-  (str (string/join " " (cons "import" iv)) "\n"))
+(defn import-atom->qml
+  "    symbol ;=> \"symbol\"
+       \"string\" ;=> \"\"string\"\"
+       :keyword ;=> \"keyword\"
+       \"1.0\" ;=> 1.0"
+  [a]
+  (str (cond
+         (symbol? a) a
+         (string? a) (if (re-find #"\d+.\d+" a)
+                       a ; version number
+                       (str "\"" a "\""))
+         (keyword? a) (name a))))
 
-(defn edn-imports->qml [eimp]
-  (str (string/join (map import-vec->str (rest eimp))) "\n"))
+(defn import-vec->qml
+  "    [Namespace \"VersionMajor.VersionMinor\"]
+       ;=> \"import Namespace VersionMajor.VersionMinor\"
+       [Namespace \"VersionMajor.VersionMinor\" :as SingletonTypeIdentifier]
+       ;=> \"import Namespace VersionMajor.VersionMinor as SingletonTypeIdentifier\"
+       [\"directory\"]
+       ;=> \"import \"directory\"\"
+       [\"file.js\" :as ScriptIdentifier]
+       ;=> \"import \"file.js\" as ScriptIdentifier\""
+  [iv]
+  (str
+    (string/join " " (cons "import" (map import-atom->qml iv)))
+    "\n"))
+
+(defn edn-imports->qml
+  "eimp is of the form (import & import-vecs). Create the corresponding QML
+   import forms. For details see the documentation for import-vec->str."
+  [eimp]
+  (str
+    (string/join (map import-vec->qml (rest eimp)))
+    "\n"))
 
 ;;; UI tree section
 ;;; ============================================================================
 
+(defn component-name?
+  "Is cn a valid component name (a symbol, string or keyword)?"
+  [cn]
+  (or (symbol? cn) (string? cn) (keyword? cn)))
+
+(defn component-name->str
+  "    cn ;=> \"cn\"
+       \"cn\" ;=> \"cn\"
+       :cn ;=> \"cn\""
+  [cn]
+  (cond
+    (or (symbol? cn) (string? cn)) (str cn)
+    (keyword? cn) (str (name cn))))
+
+(defn qt-item?
+  "Is m a qt-item, i.e. a vector whose first entry is a component-name?"
+  [m]
+  (and (vector? m) (component-name? (first m))))
+
 (defn indent-str [i]
   (string/join (repeat i "  ")))
 
-(defn qt-item? [m]
-  (and (map? m) (single? m) (symbol? (first-key m))))
-
-(declare item-map->qml)
-
-;; FIXME: catch NullPointerException
-(defn trim-semicolons [s]
-  (if (<= ((frequencies s) \newline) 1)
-    (string/replace s #";\n" "")
-    s))
-
 (defn key->qml [k]
   (if (keyword? k)
-    (keyword->str k)
+    (name k)
     (str k)))
 
 (declare qt-item->qml)
 
 (defn val->qml [v indent]
   (cond (qt-item? v) (qt-item->qml v (inc indent) :inline)
-        (symbol? v) (str v)
-        :else (trim-semicolons (compile-expr v))))
+        (string? v) (str "\"" v "\"")
+        :else (str v)))
 
 (defn items->qml [items indent]
   (if (vector? items)
     (string/join (map #(str (qt-item->qml % (inc indent) :block)) items))
     (str (qt-item->qml items (inc indent) :block))))
 
-(defn item-map-entry->qml [[k v] indent]
+(defn item-map-entry->qml
+  "Turn one item map entry [:key value] into qml."
+  [[k v] indent]
   (case k
     :Items (items->qml v indent)
     :functions nil
     (str (indent-str (inc indent))
-           (key->qml k) (when-not (nil? v) ": ")
-           (val->qml v indent) "\n")))
+         (key->qml k) (when-not (nil? v) ": ")
+         (val->qml v indent) "\n")))
 
-(defn item-map->qml [m indent]
+(defn item-map->qml
+  "Turn the property map { :id ...} into qml id: ..."
+  [m indent]
   (string/join (map #(item-map-entry->qml % indent) m)))
 
 (defn qt-item->qml [im indent inline]
-  (let [item (first-key im)
-        props (im item)]
-    (str (when (= inline :block) (indent-str indent))
-         item " {\n"
-         (item-map->qml props indent)
-         (indent-str indent) "}\n")))
+  (let [item (component-name->str (first im))
+        prop-maps (filter map? im)
+        sub-items (filter qt-item? im)]
+    (if-let [id (second (re-find #"#(\S*)" item))]
+      (recur (vec (concat
+                    [(string/replace item #"#\S*" "") {:id (symbol id)}]
+                    (rest im)))
+             indent inline)
+      (str (when (= inline :block) (indent-str indent))
+           item " {\n"
+           (string/join (map #(item-map->qml % indent) prop-maps))
+           (string/join (map #(qt-item->qml % (inc indent) inline) sub-items))
+           (indent-str indent) "}\n"))))
 
 (defn edn-ui-tree->qml [eut]
   (qt-item->qml eut 0 :block))
@@ -81,11 +118,27 @@
 ;;; API
 ;;; ============================================================================
 
-(defn edn->qml
-  ([fl]
-   (with-open [infile (java.io.PushbackReader. (clojure.java.io/reader fl))]
-     (let [imports (edn/read infile)
-           ui-tree (edn/read infile)]
-       (str (edn-imports->qml imports)
-            (edn-ui-tree->qml ui-tree)))))
-  ([ifl ofl] (spit ofl (edn->qml ifl))))
+(defn edn->QML
+  ([ui-tree] (edn-ui-tree->qml ui-tree))
+  ([imports ui-tree] (str (edn-imports->qml imports)
+                          (edn-ui-tree->qml ui-tree))))
+
+(defn to-QML [qml]
+  (str "import QtQuick 2.2\n\n" qml))
+
+(defn list-element [name]
+  (str "ListElement { name: \"" name "\"\n}\n"))
+
+(defn pc-list-element [pc]
+  (str "ListElement { pc: " pc "\n}\n"))
+
+(defn list-model [props id]
+  (str "ListModel {\n"
+       (if id
+         (str "id: " id "\n")
+         "")
+       (reduce #(str %1 %2) props)
+       "}\n"))
+
+
+
--- test/qdn/core_test.clj
+++ test/qdn/core_test.clj
@@ -2,6 +2,65 @@
   (:require [clojure.test :refer :all]
             [qdn.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest import-form
+  (is (= (edn-imports->qml
+           '(import [Namespace "1.0"]
+                    [Namespace "1.0"
+                     :as SingletonTypeIdentifier]
+                    ["directory"]
+                    ["file.js" :as ScriptIdentifier]))
+         (str "import Namespace 1.0\n"
+              "import Namespace 1.0 as SingletonTypeIdentifier\n"
+              "import \"directory\"\n"
+              "import \"file.js\" as ScriptIdentifier\n\n"))))
+
+(deftest item-form
+  (testing "component-name"
+    (are [cn] (= (component-name->str cn) "Component")
+              'Component
+              "Component"
+              :Component))
+  (testing "empty form"
+    (is (= (qt-item->qml '[Component] 0 :block)
+           "Component {\n}\n")))
+  (testing "id"
+    (testing "normal"
+      (is (= (qt-item->qml '[Component {:id ego}] 0 :block)
+             "Component {\n  id: ego\n}\n")))
+    (testing "shorthand"
+      (is (= (qt-item->qml '[Component#ego] 0 :block)
+             "Component {\n  id: ego\n}\n"))))
+  (testing "property maps"
+    (testing "single map"
+      (is (= (qt-item->qml '[Rectangle
+                             {:id ego
+                              :color "red"
+                              :border.radius 10}]
+                           0 :block)
+             "Rectangle {\n  id: ego\n  color: \"red\"\n  border.radius: 10\n}\n")))
+    (testing "multiple maps"
+      (is (= (qt-item->qml '[Rectangle {:id ego}
+                             {:color "red"
+                              :border.radius 10}]
+                           0 :block)
+             "Rectangle {\n  id: ego\n  color: \"red\"\n  border.radius: 10\n}\n"))))
+  (testing "nested forms"
+    (testing "simple"
+      (is (= (qt-item->qml '[ListModel
+                             {:id topModel}
+
+                             [ListElement {:name "has great fame"}]
+                             [ListElement {:name "what a shame"}]]
+                           0 :block)
+             (str "ListModel {\n  id: topModel\n"
+                  "  ListElement {\n    name: \"has great fame\"\n  }\n"
+                  "  ListElement {\n    name: \"what a shame\"\n  }\n"
+                  "}\n"))))
+    (testing "deeper"
+      (is (= (qt-item->qml '[Rectangle [MouseArea [Text]]] 0 :block)
+             (str "Rectangle {\n"
+                  "  MouseArea {\n"
+                  "    Text {\n"
+                  "    }\n"
+                  "  }\n"
+                  "}\n"))))))
